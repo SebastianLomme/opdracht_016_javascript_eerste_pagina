9f4c1a590479aaa6992c8859f8ed842cf7bf5899
SebastianLomme <77025210+SebastianLomme@users.noreply.github.com>
2021-08-04 12:36:16 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+let number1 = 4
+let number2 = 4
+let number3 = number1 + number2
+console.log( typeof(number3))
+
+console.log(4*4)
+console.log(4/4)
+console.log(number1 + number2)
+console.log(number3 - number1)
+console.log(20 % 110)
+
